26145494b62309417dd57fe5ac4ee8f6d5da824a
Ruslan Sadykov <ruslan.sn@icloud.com>
2020-12-01 16:03:52 +0300

    Initial Commit

---
+++ CreateParty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreateParty
+//
+//  Created by Руслан Садыков on 01.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
