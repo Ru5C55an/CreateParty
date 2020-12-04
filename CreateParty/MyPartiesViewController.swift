e905075509e53952a649df467ccbd1368b026d0e
Ruslan Sadykov <ruslan.sn@icloud.com>
2020-12-04 19:35:53 +0300

    Added Icon image, Launch Screen image, and image for cells

--- CreateParty/MyPartiesViewController.swift
+++ CreateParty/MyPartiesViewController.swift
@@ -29,6 +29,8 @@ class MyPartiesViewController: UIViewController {
 
 extension MyPartiesViewController: UITableViewDataSource, UITableViewDelegate {
     
+    // MARK: - Table view data source
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return parties.count
     }
@@ -36,7 +38,16 @@ extension MyPartiesViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
         cell?.textLabel?.text = parties[indexPath.row]
+        cell?.imageView?.image = UIImage(named: "shit")
+        cell?.imageView?.layer.cornerRadius = cell?.frame.size.height / 2
+        cell?.imageView?.clipsToBounds = true
         return cell!
+    }
+    
+    // MARK: - Table view delegate
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
     
 }
